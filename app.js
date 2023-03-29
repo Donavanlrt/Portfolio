f3e83f4d087b8a53d808871de376c09507742687
dona <donavanlrt@gmail.com>
2023-03-29 17:05:05 +0200

    update problem first block

--- app.js
+++ app.js
@@ -1,17 +1,3 @@
-// header scroll //
-
-function scrollValue() {
-    var navbar = document.getElementById('navbar');
-    var scroll = window.scrollY;
-    if (scroll < 120) {
-        navbar.classList.remove('BgColour');
-    } else {
-        navbar.classList.add('BgColour');
-    }
-}
-
-window.addEventListener('scroll', scrollValue);
-
 // text typing effect 
 
 const dynamictext = document.querySelector(".dynamic-text");
